12c20e11c7931981d7273e1bca92b209e8380b4a
higorj <higor_sgomes@outlook.com>
2022-05-06 19:22:11 -0300

    Atividade semanal - Gerenciador de Contatos

--- App.js
+++ App.js
@@ -1,20 +1,78 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, TextInput, Text, View, Button, FlatList } from 'react-native';
 
 export default function App() {
+  const [contador, setContador] = useState(0);
+  const [nome, setNome] = useState("");
+  const [telefone, setTelefone] = useState("");
+  const [contatos, setContatos] = useState([]);
+
+  const adicionarContato = () => {
+    setContatos([{
+      key: contador,
+      nome,
+      telefone
+    }, ...contatos]);
+
+    setNome("");
+    setTelefone("");
+    setContador(contador + 1);
+    console.log(contatos)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <TextInput
+          placeholder='Nome'
+          value={nome}
+          onChangeText={setNome}
+          style={styles.input}
+        />
+        <TextInput
+          placeholder='Telefone'
+          value={telefone}
+          onChangeText={setTelefone}
+          style={styles.input}
+        />
+        <Button
+          disabled={nome.length == 0 || telefone.length == 0}
+          title="Adicionar Contato"
+          onPress={adicionarContato}
+        />
+      </View>
+
+      <FlatList 
+        data={contatos}
+        renderItem={contato => (
+          <View key={contato.item.key} style={styles.listItem}>
+            <Text style={styles.textItem}>{contato.item.nome} - {contato.item.telefone}</Text>
+          </View>
+        )}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 40
   },
+  input: {
+    borderBottomColor: 'black', 
+    borderBottomWidth: 1, 
+    marginBottom: 4, 
+    padding: 12 
+  },
+  listItem: {
+    padding: 12,
+    backgroundColor: '#03a5fc',
+    borderColor: '#03a5dd',
+    borderWidth: 1,
+    borderRadius: 8,
+    marginTop: 8
+  },
+  textItem: {
+    color: '#eee'
+  }
 });
